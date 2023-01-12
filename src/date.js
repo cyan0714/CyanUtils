470689815116bf73848d8d2f1ed8fa761e0ef080
cyan0714 <917244295@qq.com>
2023-01-12 09:40:39 +0800

    add two function

--- src/date.js
+++ src/date.js
@@ -93,11 +93,26 @@ const getLastMonth = (type) => {
 
 }
 
+/**
+ * @param {number} time
+ */
+const timeAgo = (time) => {
+  const between = Date.now() / 1000 - Number(time)
+  if (between < 3600) {
+    return pluralize(~~(between / 60), ' minute')
+  } else if (between < 86400) {
+    return pluralize(~~(between / 3600), ' hour')
+  } else {
+    return pluralize(~~(between / 86400), ' day')
+  }
+}
+
 export default {
   calcDate,
   dateFormat,
   nowTime,
   getYesterday,
-  getLastMonth
+  getLastMonth,
+  timeAgo
 }
 
